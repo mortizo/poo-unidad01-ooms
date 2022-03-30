b57dd387122846ed9bc047024fabfb88af0dae2c
mortizo <mortizo@ups.edu.ec>
2022-03-30 07:38:47 -0500

    Cambio el mensaje de hola mundo al español

---
+++ src/main/java/ups/edu/ec/poo/unidad01/ooms/PooUnidad01Ooms.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ups.edu.ec.poo.unidad01.ooms;
+
+/**
+ *
+ * @author morti
+ */
+public class PooUnidad01Ooms {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        
+    }
+}
