3d57e02b64f6df69fb8593c240b8c68f31530170
mortizo <mortizo@ups.edu.ec>
2022-03-30 08:32:00 -0500

    Clase estudiante sin métodos

---
+++ src/main/java/ups/edu/ec/poo/unidad01/ooms/Estudiante.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ups.edu.ec.poo.unidad01.ooms;
+
+/**
+ *
+ * @author morti
+ */
+public class Estudiante {
+    
+    String nombre;
+    int calificacion;
+    String carrera;
+    double peso;
+   
+}
