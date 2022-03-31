b3646bb67690ee68931fe6263af299e28b28584f
mortizo <mortizo@ups.edu.ec>
2022-03-31 11:14:57 -0500

    Clase estudiante con métodos y estructuras condicionales

--- src/main/java/ups/edu/ec/poo/unidad01/ooms/Estudiante.java
+++ src/main/java/ups/edu/ec/poo/unidad01/ooms/Estudiante.java
@@ -14,5 +14,49 @@ public class Estudiante {
     int calificacion;
     String carrera;
     double peso;
+    int altura;
+    
+    
+    public String equivalenciaCalificacion()
+    {
+        var retorno="Insuficiente";
+        
+        if(this.calificacion>=90){
+            retorno="Sobresaliente";
+        }else{
+            if(this.calificacion>=80){
+                retorno="Muy buena";
+            }else{
+                if(this.calificacion>=70){
+                    retorno="Buena";
+                }
+            }
+        }
+        
+        return retorno;
+    }
+    
+    public boolean aprobado(){
+        var retorno=false;
+        
+        retorno=(this.calificacion>=70)?true:false;
+        
+        return retorno;
+    }
+    
+    public double calcularImc()
+    {
+        
+        /*
+            Fórmula IMC: peso (kg) / [estatura (m)]2
+            Ejemplo: Peso = 68 kg, Estatura = 165 cm (1.65 m)
+            Cálculo: 68 ÷ (1.65)2 = 24.98
+        */
+        var retorno=0d;
+       
+        retorno=this.peso/(this.altura^2);
+       
+        return retorno;
+    }
    
 }
