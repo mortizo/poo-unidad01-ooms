c411a8e2f79b4b87b6413a227d24b06901c62c99
mortizo <mortizo@ups.edu.ec>
2022-06-16 14:46:03 -0500

    Se agrega el paquete P60

--- src/main/java/p60/PooUnidad01Ooms.java
+++ src/main/java/p60/PooUnidad01Ooms.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
-package ups.edu.ec;
+package p60;
 
 import java.time.LocalDate;
 import java.time.Month;
