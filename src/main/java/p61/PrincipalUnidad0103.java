4070546c1a8ba266edf1db355951f7d713aedf35
mortizo <mortizo@ups.edu.ec>
2022-10-19 12:14:17 -0500

    arreglo ejemplo enteros y decimales

--- src/main/java/p61/PrincipalUnidad0103.java
+++ src/main/java/p61/PrincipalUnidad0103.java
@@ -32,17 +32,58 @@ public class PrincipalUnidad0103 {
         a++;
         System.out.println("a = " + a);
 
-        while (a<100) {
+        while (a < 100) {
             System.out.println("a = " + a);
             a++;
         }
+        var numero=0;
         //leer datos del teclado
+        do {
+            var lectura = new Scanner(System.in);
+            System.out.println("Ingresar números menores a 10");
+            numero = lectura.nextInt();
+            System.out.println("numero = " + numero);
+        } while (numero < 10);
         
-        var lectura = new Scanner(System.in);
-        System.out.println("Ingresar números menores a 10");
-        var numero=lectura.nextInt();
-        System.out.println("numero = " + numero);
-
+        
+        for (var i = 0; i < 10; i++) {
+            System.out.println("Hola "+i);
+        }
+        
+        var numeroList = new int[5];
+        numeroList[0]=7;
+        numeroList[1]=9;
+        numeroList[2]=4;
+        numeroList[3]=11;
+        numeroList[4]=3;
+        
+        var numeroDecimalList = new double[5];
+        numeroDecimalList[0]=8.5;
+        numeroDecimalList[1]=4.0;
+        numeroDecimalList[2]=9.3;
+        numeroDecimalList[3]=6.4;
+        numeroDecimalList[4]=3.9;
+        
+        var i=0;
+        System.out.println(numeroList[i]);
+        i++;
+        System.out.println(numeroList[i]);
+        i++;
+        System.out.println(numeroList[i]);
+        System.out.println(numeroList[3]);
+        System.out.println(numeroList[4]);
+        
+        System.out.println(numeroDecimalList[0]);
+        System.out.println(numeroDecimalList[1]);
+        System.out.println(numeroDecimalList[2]);
+        System.out.println(numeroDecimalList[3]);
+        System.out.println(numeroDecimalList[4]);
+        
+        
+        
+        
+        
+        
     }
 
 }
